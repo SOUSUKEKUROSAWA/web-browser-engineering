e9d77a04086f8d6d9ec4ebaff483b5f0b546d27a
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2026-06-28 16:50:58 +0900

    1.5 リクエストとレスポンス

--- browser/main.py
+++ browser/main.py
@@ -20,3 +20,33 @@ class URL:
             proto=socket.IPPROTO_TCP, # プロトコル（＝ 2台のコンピュータが接続を確立する方法）．
         )
         s.connect((self.host, 80))
+
+        # リクエストの送信．
+        request = "GET {} HTTP/1.0\r\n".format(self.path)
+        request += "Host: {}\r\n".format(self.host)
+        request += "\r\n"
+        s.send(request.encode("utf8"))
+
+        # レスポンスの受信．
+        response = s.makefile("r", encoding="utf8", newline="\r\n") # レスポンスのバイトストリームを1つのファイルオブジェクトにまとめて返す．
+
+        # レスポンスのパース（1行目）．
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+
+        # レスポンスのパース（ヘッダー）．
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n": break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip() # casefold(): 小文字に正規化．strip(): 前後の空白を削除．
+
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+
+        # レスポンスのパース（ボディ）．
+        content = response.read()
+        s.close()
+
+        return content
