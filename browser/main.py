166b0a0fbaeb0f9c735aba20026cccc9055f67f3
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2026-06-28 19:47:48 +0900

    nits

--- browser/main.py
+++ browser/main.py
@@ -2,7 +2,7 @@ import socket
 import ssl
 
 class URL:
-    def __init__(self, url):
+    def __init__(self, url: str):
         # URL のパース．
         self.scheme, url = url.split("://", 1)
         assert self.scheme in ["http", "https"]
