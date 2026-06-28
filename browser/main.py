640b698777f9434b41ca381717d4f38dfe10a5d9
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2026-06-28 17:08:53 +0900

    1.6 HTML の表示

--- browser/main.py
+++ browser/main.py
@@ -50,3 +50,21 @@ class URL:
         s.close()
 
         return content
+
+def show(body):
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            print(c, end="")
+
+def load(url: URL):
+    body = url.request()
+    show(body)
+
+if __name__ == "__main__":
+    import sys
+    load(URL(sys.argv[1]))
