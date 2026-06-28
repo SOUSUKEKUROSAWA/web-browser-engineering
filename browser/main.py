1094f711b1536becf27bd56f85cc582b4c80dece
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2026-06-28 13:10:09 +0900

    1.4 Python での Telnet

---
+++ browser/main.py
@@ -0,0 +1,22 @@
+import socket
+
+class URL:
+    def __init__(self, url):
+        # URL のパース．
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme == "http"
+
+        if "/" not in url:
+            url = url + "/"
+
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+
+    def request(self):
+        # ソケットの作成．
+        s = socket.socket(
+            family=socket.AF_INET, # アドレスファミリー（＝ 他のコンピュータを見つける方法）．INET（＝ IPv4 を使用）．
+            type=socket.SOCK_STREAM, # ソケットタイプ（＝ 行われる会話の種類）．STREAM（＝ 任意の量のデータを送信できる）．
+            proto=socket.IPPROTO_TCP, # プロトコル（＝ 2台のコンピュータが接続を確立する方法）．TCP．
+        )
+        s.connect((self.host, 80))
