20034d81106000ce9d0bf82c5632b4c2d90f1254
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2026-07-16 22:48:21 +0900

    refactor: 並び替え

--- browser/main.py
+++ browser/main.py
@@ -4,19 +4,6 @@ import tkinter.font
 import socket
 import ssl
 
-WIDTH = 800
-"""キャンバス全体の幅"""
-HEIGHT = 600
-"""キャンバス全体の高さ"""
-HSTEP = 13
-"""画面上の1文字の幅"""
-VSTEP = 18
-"""画面上の1文字の高さ"""
-SCROLL_STEP = 100
-"""1回の画面スクロールで座標が移動する距離"""
-FONTS = {}
-"""フォントキャッシュ"""
-
 class URL:
     def __init__(self, url: str):
         # URL のパース．
@@ -81,50 +68,6 @@ class URL:
 
         return content
 
-class Browser:
-    def __init__(self):
-        self.window = tkinter.Tk()
-        self.canvas = tkinter.Canvas(
-            self.window,
-            width=WIDTH,
-            height=HEIGHT
-        )
-        self.canvas.pack()
-        self.scroll = 0
-        self.window.bind("<Down>", self.scrolldown) # 下矢印キーがクリックされたら，scrolldown メソッドが呼ばれる．
-
-    def draw(self):
-        """
-        テキストの画面座標を決定し，画面（キャンバス）に描画する．
-
-        ページ座標：Webページ全体における位置座標
-        画面座標：画面（フレーム）内における位置座標
-
-        e.g. ページ座標(y) 123 ピクセルの位置のテキストが 30 ピクセル下にスクロールされた場合の画面座標(y)は 93 ピクセル．
-        """
-        self.canvas.delete("all") # 再描画時のためにまずキャンバスをクリア．
-        for x, y, word, font in self.display_list:
-            if y > self.scroll + HEIGHT: continue # 画面下部より下の文字
-            if y + VSTEP < self.scroll: continue # 画面上部より上の文字
-
-            self.canvas.create_text(
-                x,
-                y - self.scroll, # 左上が（0,0）右下が（x,y）となる座標系．
-                text=word,
-                font=font,
-                anchor="nw" # north west の略．左上の角を（0,0）として描画する．
-            )
-
-    def load(self, url: URL):
-        body = url.request()
-        self.nodes = HTMLParser(body).parse()
-        self.display_list = Layout(self.nodes).display_list
-        self.draw()
-
-    def scrolldown(self, e):
-        self.scroll += SCROLL_STEP
-        self.draw() # 再描画
-
 class Text:
     def __init__(self, text, parent):
         self.text: str = text
@@ -144,126 +87,10 @@ class Element:
     def __repr__(self):
         return "<" + self.tag + ">"
 
-class Layout:
-    def __init__(self, tree: Element):
-        self.display_list = []
-        """ページ座標やフォント情報を保持するリスト"""
-        self.cursor_x = HSTEP
-        self.cursor_y = VSTEP
-        self.weight = "normal"
-        self.style = "roman"
-        self.size = 12
-        self.line: list[tuple[int, str, tkinter.font.Font]] = []
-        """行バッファ"""
-
-        self.recurse(tree)
-
-        # 最終行のフォーマット
-        self.flush()
-
-    def flush(self):
-        """
-        改行の前処理
-
-        その行のベースラインの計算と，改行後のカーソル位置の計算．
-        """
-        if not self.line: return # 空の行はスキップ
-
-        # ベースラインに沿って単語を整列させる（テキストのサイズが異なるとバラバラになってしまうため）
-        max_ascent = max([font.metrics("ascent") for x, word, font in self.line]) # 行内のテキストの最大アセント（高さ）
-        baseline = self.cursor_y + 1.25 * max_ascent
-        for x, word, font in self.line:
-            y = baseline - font.metrics("ascent") # ベースラインからアセント（高さ）の分だけ上が，このテキストの左上になる．
-            self.display_list.append((x, y, word, font))
-
-        # 次の行の x, y 座標をセット
-        metrics = [font.metrics() for x, word, font in self.line]
-        max_descent = max([metric["descent"] for metric in metrics]) # 行内のテキストの最大ディセント（g や y などの文字の下側のはみ出し部分の深さ）
-        self.cursor_y = baseline + 1.25 * max_descent
-        self.cursor_x = HSTEP
-        self.line = [] # バッファをクリア
-
-    def recurse(self, tree: Union[Text, Element]):
-        """
-        ツリーを再帰的にパースする．
-        """
-        if isinstance(tree, Text):
-            for word in tree.text.split():
-                self.word(word)
-        else:
-            self.open_tag(tree.tag)
-            for child in tree.children:
-                self.recurse(child)
-            self.close_tag(tree.tag)
-
-    def open_tag(self, tag):
-        if tag == "i":
-            self.style = "italic"
-        elif tag == "b":
-            self.weight = "bold"
-        elif tag == "small":
-            self.size -= 2
-        elif tag == "big":
-            self.size += 4
-        elif tag == "br":
-            self.flush()
-
-    def close_tag(self, tag):
-        if tag == "/i":
-            self.style = "roman"
-        elif tag == "/b":
-            self.weight = "normal"
-        elif tag == "/small":
-            self.size += 2
-        elif tag == "/big":
-            self.size -= 4
-        elif tag == "/p":
-            self.flush()
-            self.cursor_y += VSTEP # 段落間のスペースを追加
-
-    def token(self, tok):
-        if isinstance(tok, Text):
-            for word in tok.text.split():
-                self.word(word)
-        elif isinstance(tok, Element):
-            if tok.tag == "i":
-                self.style = "italic"
-            elif tok.tag == "/i":
-                self.style = "roman"
-            elif tok.tag == "b":
-                self.weight = "bold"
-            elif tok.tag == "/b":
-                self.weight = "normal"
-            elif tok.tag == "small":
-                self.size -= 2
-            elif tok.tag == "/small":
-                self.size += 2
-            elif tok.tag == "big":
-                self.size += 4
-            elif tok.tag == "/big":
-                self.size -= 4
-            elif tok.tag == "br":
-                self.flush()
-            elif tok.tag == "/p":
-                self.flush()
-                self.cursor_y += VSTEP # 段落間のスペースを追加
-
-    def word(self, word):
-        """
-        個々の単語のディスプレイリストを作成する．
-        """
-        font = get_font(self.size, self.weight, self.style)
-        w = font.measure(word) # 英語は単語ごとにサイズが異なるので，都度幅を計算する．
-
-        if self.cursor_x + w > WIDTH - HSTEP:
-            # 折り返し
-            self.flush()
-
-        # その行のディスプレイリストをバッファに追加
-        self.line.append((self.cursor_x, word, font))
-
-        # x カーソルを次の単語まで移動
-        self.cursor_x += w + font.measure(" ")
+def print_tree(node: Union[Text, Element], indent=0):
+    print(" " * indent, node)
+    for child in node.children:
+        print_tree(child, indent + 2)
 
 class HTMLParser:
     SELF_CLOSING_TAGS = ["area","base","br","col","embed","hr","img","input","link","meta","param","source","track","wbr",]
@@ -394,6 +221,9 @@ class HTMLParser:
             else:
                 break
 
+FONTS = {}
+"""フォントキャッシュ"""
+
 def get_font(size, weight, style) -> tkinter.font.Font:
     """
     フォントキャッシュからフォントを取得，または作成する．
@@ -410,10 +240,155 @@ def get_font(size, weight, style) -> tkinter.font.Font:
 
     return FONTS[key][0]
 
-def print_tree(node: Union[Text, Element], indent=0):
-    print(" " * indent, node)
-    for child in node.children:
-        print_tree(child, indent + 2)
+WIDTH = 800
+"""キャンバス全体の幅"""
+HEIGHT = 600
+"""キャンバス全体の高さ"""
+HSTEP = 13
+"""画面上の1文字の幅"""
+VSTEP = 18
+"""画面上の1文字の高さ"""
+
+class Layout:
+    def __init__(self, tree: Element):
+        self.display_list = []
+        """ページ座標やフォント情報を保持するリスト"""
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+        self.line: list[tuple[int, str, tkinter.font.Font]] = []
+        """行バッファ"""
+
+        self.recurse(tree)
+
+        # 最終行のフォーマット
+        self.flush()
+
+    def flush(self):
+        """
+        改行の前処理
+
+        その行のベースラインの計算と，改行後のカーソル位置の計算．
+        """
+        if not self.line: return # 空の行はスキップ
+
+        # ベースラインに沿って単語を整列させる（テキストのサイズが異なるとバラバラになってしまうため）
+        max_ascent = max([font.metrics("ascent") for x, word, font in self.line]) # 行内のテキストの最大アセント（高さ）
+        baseline = self.cursor_y + 1.25 * max_ascent
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent") # ベースラインからアセント（高さ）の分だけ上が，このテキストの左上になる．
+            self.display_list.append((x, y, word, font))
+
+        # 次の行の x, y 座標をセット
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_descent = max([metric["descent"] for metric in metrics]) # 行内のテキストの最大ディセント（g や y などの文字の下側のはみ出し部分の深さ）
+        self.cursor_y = baseline + 1.25 * max_descent
+        self.cursor_x = HSTEP
+        self.line = [] # バッファをクリア
+
+    def recurse(self, tree: Union[Text, Element]):
+        """
+        ツリーを再帰的にパースする．
+        """
+        if isinstance(tree, Text):
+            for word in tree.text.split():
+                self.word(word)
+        else:
+            self.open_tag(tree.tag)
+            for child in tree.children:
+                self.recurse(child)
+            self.close_tag(tree.tag)
+
+    def open_tag(self, tag):
+        if tag == "i":
+            self.style = "italic"
+        elif tag == "b":
+            self.weight = "bold"
+        elif tag == "small":
+            self.size -= 2
+        elif tag == "big":
+            self.size += 4
+        elif tag == "br":
+            self.flush()
+
+    def close_tag(self, tag):
+        if tag == "/i":
+            self.style = "roman"
+        elif tag == "/b":
+            self.weight = "normal"
+        elif tag == "/small":
+            self.size += 2
+        elif tag == "/big":
+            self.size -= 4
+        elif tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP # 段落間のスペースを追加
+
+    def word(self, word):
+        """
+        個々の単語のディスプレイリストを作成する．
+        """
+        font = get_font(self.size, self.weight, self.style)
+        w = font.measure(word) # 英語は単語ごとにサイズが異なるので，都度幅を計算する．
+
+        if self.cursor_x + w > WIDTH - HSTEP:
+            # 折り返し
+            self.flush()
+
+        # その行のディスプレイリストをバッファに追加
+        self.line.append((self.cursor_x, word, font))
+
+        # x カーソルを次の単語まで移動
+        self.cursor_x += w + font.measure(" ")
+
+SCROLL_STEP = 100
+"""1回の画面スクロールで座標が移動する距離"""
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown) # 下矢印キーがクリックされたら，scrolldown メソッドが呼ばれる．
+
+    def draw(self):
+        """
+        テキストの画面座標を決定し，画面（キャンバス）に描画する．
+
+        ページ座標：Webページ全体における位置座標
+        画面座標：画面（フレーム）内における位置座標
+
+        e.g. ページ座標(y) 123 ピクセルの位置のテキストが 30 ピクセル下にスクロールされた場合の画面座標(y)は 93 ピクセル．
+        """
+        self.canvas.delete("all") # 再描画時のためにまずキャンバスをクリア．
+        for x, y, word, font in self.display_list:
+            if y > self.scroll + HEIGHT: continue # 画面下部より下の文字
+            if y + VSTEP < self.scroll: continue # 画面上部より上の文字
+
+            self.canvas.create_text(
+                x,
+                y - self.scroll, # 左上が（0,0）右下が（x,y）となる座標系．
+                text=word,
+                font=font,
+                anchor="nw" # north west の略．左上の角を（0,0）として描画する．
+            )
+
+    def load(self, url: URL):
+        body = url.request()
+        self.nodes = HTMLParser(body).parse()
+        self.display_list = Layout(self.nodes).display_list
+        self.draw()
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw() # 再描画
 
 if __name__ == "__main__":
     import sys
